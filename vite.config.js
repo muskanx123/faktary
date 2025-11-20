79bd94106fbddcd931bf63c22e6d08046b2a94d4
Muskan <mus2002123@gmail.com>
2025-11-20 12:10:15 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/faktaryesolution-website/",
-});
+  base: '/faktary/',   // 👈 yahan sirf repo ka naam
+})
