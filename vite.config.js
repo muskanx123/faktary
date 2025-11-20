f6a2d168e9e5c942df27f14e0502b69b30da262b
Muskan <mus2002123@gmail.com>
2025-11-20 12:26:00 +0530

    build ready

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/faktary/',   // 👈 yahan sirf repo ka naam
+  base: '/faktary/',   // 👈 repo ka naam
 })
